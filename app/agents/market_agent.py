97cbb0e0e55c2bcbcf235c6c5a99df3fb7435de1
priyankabanda <89898380+priyankabanda2202@users.noreply.github.com>
2026-06-05 12:26:09 +0530

    Update market_agent.py

--- app/agents/market_agent.py
+++ app/agents/market_agent.py
@@ -1,28 +1,11 @@
 from app.tools.market_data import fetch_market_data
-from app.tools.indicators import calculate_rsi
 
 def market_agent(state):
 
     ticker = state["ticker"]
 
-    try:
-        market = fetch_market_data(ticker)
-
-    except Exception as e:
-
-        print(f"Market Agent Error: {e}")
-
-        market = {}
-
-    try:
-        rsi = calculate_rsi(ticker)
-    except Exception:
-        rsi = "N/A"
+    market = fetch_market_data(ticker)
 
     return {
-        "market": {
-            "price": market.get("price", "N/A"),
-            "pe_ratio": market.get("pe_ratio", "N/A"),
-            "rsi": rsi
-        }
+        "market": market
     }
