9e6366404314bfbaf7d6eb932f226d845ad66016
priyankabanda <89898380+priyankabanda2202@users.noreply.github.com>
2026-06-06 08:20:41 +0530

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -1,11 +1,46 @@
 import os
+
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, MessageHandler, filters
-from app.telegram_bot import handle_message
+
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    CommandHandler,
+    filters
+)
+
+from app.telegram_bot import (
+    handle_message,
+    start
+)
+
+# Load .env
 load_dotenv()
+
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-#print(BOT_TOKEN)
+
+if not BOT_TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN not found in .env")
+
+# Create Telegram Application
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+# /start command
+app.add_handler(
+    CommandHandler(
+        "start",
+        start
+    )
+)
+
+# Handle ticker messages
+app.add_handler(
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        handle_message
+    )
+)
+
+print("🚀 Ticker Research Bot Started")
 
 app.run_polling()
